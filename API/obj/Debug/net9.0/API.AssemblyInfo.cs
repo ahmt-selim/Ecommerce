79c02ad1c7cb019e9eb690c7912f0309f8072a08
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-10-24 21:37:51 +0300

    ef core design ve ef core sqlite paketleri yüklendi. DataContext oluşturuldu.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5f8d37015ce9e8b85abc3ed4dd353c88d3a8eed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0afdeae42e9de65adc2e8a8a4ed20bdd1378a4b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
