31c2a560ee215ec0853574f9bd137842fc08f9ff
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-10-24 23:38:01 +0300

    connectionString ile sqlite ecommerce.db oluşturmak için işlemler yapıldı.

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -4,5 +4,9 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Data;
 public class DataContext: DbContext
 {
+    public DataContext(DbContextOptions options) : base(options)//Buradan DataContext in kullanıldığı Program.cs deki bağlantı kısmına veri gönderdik(options)
+    {
+        
+    }
     public DbSet<Product> Products => Set<Product>(); //arrow function ile referans verip null olmamasını sağlıyoruz.
 }
