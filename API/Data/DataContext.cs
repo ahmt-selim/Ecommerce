5bca7bc26390ef022246153430b4dd281f92227e
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-10-25 00:01:37 +0300

    Veritabanına test veriler eklendi.

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -9,4 +9,18 @@ public class DataContext: DbContext
         
     }
     public DbSet<Product> Products => Set<Product>(); //arrow function ile referans verip null olmamasını sağlıyoruz.
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Product>().HasData(
+            new List<Product>
+            {
+new Product{Id=1, Name="IPhone 15", Description="Telefon Açıklaması", ImageUrl="1.jpg", Price=7000, IsActive=true,Stock=100},
+new Product{Id=2, Name="IPhone 16", Description="Telefon Açıklaması", ImageUrl="2.jpg", Price=8000, IsActive=true,Stock=100},
+new Product{Id=3, Name="IPhone 16 Pro", Description="Telefon Açıklaması", ImageUrl="3.jpg", Price=9000, IsActive=false,Stock=100},
+new Product{Id=4, Name="IPhone 15 Pro Max", Description="Telefon Açıklaması", ImageUrl="4.jpg", Price=10000, IsActive=true,Stock=100},
+            }
+        );//Program çalıştırıldığında veritabanına test verileri kaydeder.
+    }
 }
