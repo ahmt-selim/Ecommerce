a60d43fe66cf07691c4b4a299cc94a1b81b658f4
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-10-25 23:23:10 +0300

    ürün durumuna göre && operatörü ile şart eklenip ürünler filtrelendi.

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,7 +1,7 @@
 const products = [
-  {name: "product 1", price: 1000},
-  {name: "product 2", price: 1000},
-  {name: "product 3", price: 1000},
+  {id: 1, name: "product 1", price: 1000, isActive: true},
+  {id: 2, name: "product 2", price: 1000, isActive: false},
+  {id: 3, name: "product 3", price: 1000, isActive: true},
 ]
 
 function App() {
@@ -22,19 +22,27 @@ function ProductList(){
   return(// Product componentine gönderilen props yani parametre, burada kullanılan isim ile component içindeki isim ile aynı olmalıdır.(product)
     <div>
       <h2>ProductList</h2>
-      <Product product={products[0]}/>
-      <Product product={products[1]}/>
-      <Product product={products[2]}/>
+      { products.map(p => (
+        p.isActive && //Buradaki and operatörü ile p.isActive tru ise şartını ekledik.
+        <Product key={p.id} product={p}/>
+
+      ))}
+
     </div>
     
   );
 }
 function Product(props: any){
   return(
-    <div>
-      <h3>{props.product.name}</h3>
-      <p>{props.product.price}</p>
+    <>
+    {props.product.isActive && (//Bu şekilde de and operatörü ile if mantığı ile şart ekleyebiliyoruz.
+      <div>
+        <h3>{props.product.name}</h3>
+        <p>{props.product.price}</p>
     </div>
+    )}
+    
+    </>
     
   );
 }
