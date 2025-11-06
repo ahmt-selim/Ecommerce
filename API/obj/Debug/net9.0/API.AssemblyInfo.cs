0c4aeeeebe4644ec8550c2a34507de3f89fbfb89
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-11-06 21:44:20 +0300

    navbar düzenlendi. Menü buronları eklendi.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dd5263d0cd27414fa373e18be7fa128961e1ead")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+237bf29ed5526644b43a4aebf7a79b36c5dd8ddb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
