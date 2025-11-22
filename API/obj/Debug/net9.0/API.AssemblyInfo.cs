75689c4e47331e97133ce7a8a5fc7ba277822ddb
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-11-22 22:43:33 +0300

    ErrorController ile error handlik yapıldı.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de2e41309a8eee27ed3c31f8e5af1be97bf9037e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c93de04bf365d78baed015185446f527ec46ee0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
