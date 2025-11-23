9b61cbebd673ab5070f168cc975ed2afd96cd0f4
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2025-11-23 22:41:46 +0300

    500 hatası için yeni bir component eklendi. server-error routeu üzerinden gösterildi.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77351b51c43706acecf2eb146870a6f996166bb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58d34d4603934ddcf0987c760d93b8437ed42d58")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
