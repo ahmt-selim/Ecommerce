90085757a63d568b6cecb442a1f53ec2ca3f1336
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2026-08-21 20:02:07 +0300

    Cart sayfasında ürün ekleme azaltma ve tamamen silme butonları eklendi.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+985568ac9d91cbfcaada65fccadab413b688ca23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eaf36873b87d8b92efb5572a92f7796e00f1ccd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Client/src/pages/cart/ShoppingCartPage.tsx
+++ Client/src/pages/cart/ShoppingCartPage.tsx
@@ -1,14 +1,34 @@
-import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, IconButton } from "@mui/material";
-import { Delete } from "@mui/icons-material";
+import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, IconButton, Alert } from "@mui/material";
+import { AddCircleOutline, Delete, RemoveCircleOutline } from "@mui/icons-material";
 import { useCartContext } from "../../context/CartContext";
+import { LoadingButton } from "@mui/lab";
+import { useState } from "react";
+import requests from "../../api/requests";
 
 export default function ShoppingCartPAge()
 {
-    const{ cart } = useCartContext();
+    const{ cart, setCart } = useCartContext();
+    const [status, setStatus] = useState({loading: false, id: ""});
 
+    function handleAddItem(productId: number, id: string){
+      setStatus({loading: true, id: id});
 
+      requests.Cart.addItem(productId)
+        .then(cart => setCart(cart))
+        .catch(error => console.log(error))
+        .finally(() => setStatus({loading: false, id: ""}));
 
-    if(!cart) return <h1>Sepetinizde ürün yok</h1>
+    }
+    function handleDeleteItem(productId: number, id: string, quantity = 1){
+      setStatus({loading: true, id: id});
+
+      requests.Cart.deleteItem(productId, quantity)
+        .then((cart) => setCart(cart))
+        .catch(error => console.log(error))
+        .finally(() => setStatus({loading: false, id: ""}));
+    }
+
+    if(cart?.cartItems.length === 0) return <Alert severity="warning">Sepetinizde ürün yok</Alert>
 
     return (
     <TableContainer component={Paper}>
@@ -24,7 +44,7 @@ export default function ShoppingCartPAge()
           </TableRow>
         </TableHead>
         <TableBody>
-          {cart.cartItems.map((item) => (
+          {cart?.cartItems.map((item) => (
             <TableRow
               key={item.productId}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -36,10 +56,21 @@ export default function ShoppingCartPAge()
                 {item.name}
               </TableCell>
               <TableCell align="right">{item.price} ₺</TableCell>
-              <TableCell align="right">{item.quantity}</TableCell>
+              <TableCell align="right">
+                <LoadingButton loading={status.loading && status.id === "add" + item.productId} 
+                  onClick={() => handleAddItem(item.productId, "add" + item.productId)} >
+                  <AddCircleOutline />
+                </LoadingButton>
+                {item.quantity}
+                <LoadingButton loading={status.loading && status.id === "del" + item.productId} 
+                  onClick={() => handleDeleteItem(item.productId, "del" + item.productId)}>
+                  <RemoveCircleOutline />
+                </LoadingButton>
+                </TableCell>
               <TableCell align="right">{item.price * item.quantity} ₺</TableCell>
               <TableCell align="right">
-                    <IconButton color="error">
+                    <IconButton color="error" loading={status.loading && status.id === "del_all" + item.productId} 
+                      onClick={() => handleDeleteItem(item.productId, "del_all" + item.productId, item.quantity)}>
                         <Delete />
                     </IconButton>
               </TableCell>
