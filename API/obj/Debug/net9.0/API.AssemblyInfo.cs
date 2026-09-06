7e488bea0f6a5431f3f9340fd8f61d3c1caab695
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2026-09-06 15:52:12 +0300

    Kullanıcı ekleme için CreateUser metodu eklendi.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7d10727a72bc4f5aaee7952c88532f59e3803aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d21f10cd390233dc95a8d8e49003bd5ab9dcec4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
