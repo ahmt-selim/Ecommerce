be860838c664469b730318210978967ca228f919
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2026-09-06 15:59:08 +0300

    program.cs'e IdentityOptions optionları eklendi.

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entity;
 using API.Middlewares;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -17,6 +18,16 @@ builder.Services.AddDbContext<DataContext>(options =>
 
 builder.Services.AddCors();
 builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<DataContext>();
+
+builder.Services.Configure<IdentityOptions>(options => {
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
+
+    options.User.RequireUniqueEmail = true;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
