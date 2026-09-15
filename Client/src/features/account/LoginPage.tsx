612175d0791fc24c8f438101a9fe0ff6fdd8887c
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2026-09-16 00:16:39 +0300

    request.ts'e Account tipi eklendi.

--- Client/src/features/account/LoginPage.tsx
+++ Client/src/features/account/LoginPage.tsx
@@ -1,10 +1,28 @@
 import { LockOutline } from "@mui/icons-material";
 import { Avatar, Box, Button, Container, Paper, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import requests from "../../api/requests";
 
 export default function LoginPage()
 {
-    function handleSubmit(){
-        
+    // const [username, SetUsername] = useState("");
+    // const [passowrd, SetPassword] = useState("");
+
+    const [values, setValues] = useState({
+        username: "",
+        password: ""
+    });
+
+    function handleSubmit(e: any)
+    {
+        e.preventDefault();
+        console.log(values);
+        requests.Account.login(values);
+    }
+
+    function handleInputChange (e: any){
+        const{name, value} = e.target;
+        setValues({...values, [name]: value})
     }
     return (
         <Container maxWidth="xs">
@@ -14,8 +32,10 @@ export default function LoginPage()
                 </Avatar>
                 <Typography component="h1" variant="h5" sx={{textAlign: "center"}}>Login</Typography>
                 <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 2}}>
-                    <TextField label="Enter username" fullWidth required autoFocus sx={{mb:2}} size="small"></TextField>
-                    <TextField label="Enter password" type="password" fullWidth required autoFocus sx={{mb:2}} size="small"></TextField>
+
+                    <TextField name="username" value={values.username} onChange={handleInputChange} label="Enter username" fullWidth required autoFocus sx={{mb:2}} size="small"></TextField>
+                    <TextField name="password" value={values.password} onChange={handleInputChange} label="Enter password" type="password" fullWidth required autoFocus sx={{mb:2}} size="small"></TextField>
+
                     <Button type="submit" variant="contained" fullWidth sx={{mt:1}}>Login</Button>
                 </Box>
             </Paper>
--- Client/src/router/Routes.tsx
+++ Client/src/router/Routes.tsx
@@ -9,8 +9,8 @@ import ErrorPage from "../features/ErrorPage";
 import ServerError from "../errors/ServerError";
 import NotFound from "../errors/NotFound";
 import ShoppingCartPAge from "../features/cart/ShoppingCartPage";
-import LoginPage from "../features/account/LoginPage";
 import RegisterPage from "../features/account/RegisterPage";
+import LoginPage from "../features/account/LoginPage";
 
 export const router = createBrowserRouter([
     {
